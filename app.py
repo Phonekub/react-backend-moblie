13d089fa936175b40509ee65d181244acee6759b
Phonekub <155616014+Phonekub@users.noreply.github.com>
2024-03-07 20:07:54 +0000

    finish

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 cots=CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 books=[
-    {"id":1,"title":"Book 1","author":"Author 1"},
+    {"id":1,"title":"Book1","author":"Author 1"},
     {"id":2,"title":"Book 2","author":"Author 2"},
     {"id":3,"title":"Book 3","author":"Author 3"}
 ]
